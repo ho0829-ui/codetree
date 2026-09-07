90f0d0b7fc4e2e182c9eea022c5005d1db85b247
ho0829-ui <chlwogh0829@gmail.com>
2026-09-07 10:44:19 +0900

    [Passed] 369 게임 (66ms, 16MB)

---
+++ trail1/369 게임/369-game.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 3 == 0:
+        print(0, end=' ')
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 ==9:
+        print(0, end=' ')
+    elif i >= 30 and i < 40:
+        print(0, end=' ')
+    elif i >= 60 and i < 70:
+        print(0, end=' ')
+    elif i >= 90 and i < 100:
+        print(0, end=' ')
+    else:
+        print(i, end=' ')
